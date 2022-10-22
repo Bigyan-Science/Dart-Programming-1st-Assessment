c6dd3f35a693f056e5b83c4505436f87760f3a0c
Bigyan Thapa <80626045+Bigyan-Science@users.noreply.github.com>
2022-10-22 10:15:23 +0545

    Update q10.dart

--- q10.dart
+++ q10.dart
@@ -1,5 +1,5 @@
+// Write a dart program to determine whether the number is a prime number or not.
 import 'dart:io';
-
 void main() {
   var a = [1, 0, 2, 3, 5, 8, 13, 21, 34, 55, 89];
   int len = a.length;
